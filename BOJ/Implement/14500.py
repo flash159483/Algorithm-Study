65d63d0932606222eb73c64e1751be00333829dd
flash159483 <seungwonjung0313@gmail.com>
2023-02-26 21:55:47 +0900

    23/02/26

---
+++ BOJ/Implement/14500.py
@@ -0,0 +1,76 @@
+# 14500
+# Gold 4
+
+# 폴리오미노란 크기가 1×1인 정사각형을 여러 개 이어서 붙인 도형이며, 다음과 같은 조건을 만족해야 한다.
+#
+# 정사각형은 서로 겹치면 안 된다.
+# 도형은 모두 연결되어 있어야 한다.
+# 정사각형의 변끼리 연결되어 있어야 한다. 즉, 꼭짓점과 꼭짓점만 맞닿아 있으면 안 된다.
+# 정사각형 4개를 이어 붙인 폴리오미노는 테트로미노라고 하며, 다음과 같은 5가지가 있다.
+#
+#
+#
+# 아름이는 크기가 N×M인 종이 위에 테트로미노 하나를 놓으려고 한다. 종이는 1×1 크기의 칸으로 나누어져 있으며, 각각의 칸에는 정수가 하나 쓰여 있다.
+#
+# 테트로미노 하나를 적절히 놓아서 테트로미노가 놓인 칸에 쓰여 있는 수들의 합을 최대로 하는 프로그램을 작성하시오.
+#
+# 테트로미노는 반드시 한 정사각형이 정확히 하나의 칸을 포함하도록 놓아야 하며, 회전이나 대칭을 시켜도 된다.
+
+# Example
+# Input         Output
+# 5 5           19
+# 1 2 3 4 5
+# 5 4 3 2 1
+# 2 3 4 5 6
+# 6 5 4 3 2
+# 1 2 1 2 1
+
+import sys
+
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+graph = [list(map(int, input().split())) for _ in range(n)]
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+ans = 0
+
+
+def dfs(x, y, csum, cnt):
+    global ans
+    if cnt == 4:
+        ans = max(ans, csum)
+        return
+
+    for i in range(4):
+        nx, ny = x + dx[i], y + dy[i]
+        if 0 <= nx < n and 0 <= ny < m and not visited[nx][ny]:
+            visited[nx][ny] = True
+            dfs(nx, ny, csum + graph[nx][ny], cnt+1)
+            visited[nx][ny] = False
+
+
+def rest(x, y):
+    result = 0
+    for i in range(4):
+        tmp = graph[x][y]
+        for k in range(3):
+            s = (i + k) % 4
+            nx, ny = x + dx[s], y + dy[s]
+            if 0 <= nx < n and 0 <= ny < m:
+                tmp += graph[nx][ny]
+        result = max(result, tmp)
+    return result
+
+
+visited = [[False] * m for _ in range(n)]
+
+for i in range(n):
+    for j in range(m):
+        visited[i][j] = True
+        dfs(i, j, graph[i][j], 1)
+        visited[i][j] = False
+        ans = max(ans, rest(i, j))
+
+print(ans)
